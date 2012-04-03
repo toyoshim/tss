5e304dd93b6d5f01397cc22a73ebf736e0cf23eb
Takashi Toyoshima <toyoshim@gmail.com>
2012-04-03 14:47:53 +0900

    Add SoundFont2 prototype

---
+++ js/tss/SoundFont2.js
@@ -0,0 +1,21 @@
+/**
+ * T'SoundSystem for JavaScript
+ */
+
+/**
+ * SoundFont2 container base prototype
+ *
+ * Load SoundFont2 file.
+ * @author Takashi Toyoshima <toyoshim@gmail.com>
+ */
+function SoundFont2 () {
+}
+
+/**
+ * Load SoundFont2 of ArrayBuffer.
+ * @param data ArrayBuffer containing SoundFont2
+ */
+SoundFont2.prototype.load = function (data) {
+    var sf2 = new Uint8Array(data);
+    Log.getLog().info("SF2: loading sf2 data; size = " + sf2.byteLength);
+};
